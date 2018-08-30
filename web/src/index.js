142d30e1fca4ca6c8830763bf8b28e44f6823019
stephen.zambaux <stephen.zambaux@gmail.com>
2018-08-30 17:36:17 +0200

    initial commit

--- web/src/index.js
+++ web/src/index.js
@@ -3,7 +3,9 @@ import ReactDOM from 'react-dom';
 import './index.css';
 import App from './App';
 import registerServiceWorker from './registerServiceWorker';
+import 'share/src/component/component.css';
 window.mode = 'WEB';
+
 
 ReactDOM.render(<App />, document.getElementById('root'));
 registerServiceWorker();
